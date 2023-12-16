58ecbb53c2e909b01c73ea5ddaafff423647d889
arkayzdev <franck.zhuang@hotmail.com>
2023-12-16 14:33:14 +0100

    Setup API Routes

--- api/app.py
+++ api/app.py
@@ -1,14 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask
+from controller.user import user_blueprint
+from controller.reservation import reservation_blueprint
+from controller.apartment import apartment_blueprint
 
 app = Flask(__name__)
 
+app.register_blueprint(user_blueprint, url_prefix='/user')
+app.register_blueprint(reservation_blueprint, url_prefix='/reservation')
+app.register_blueprint(apartment_blueprint, url_prefix='/apartment')
 
-app.config["DEBUG"] = True
-
-
-@app.route('/', methods=['GET'])
-def home():
-   return "<h1>Annuaire Internet</h1><p>Ce site est le prototype d’une API mettant à disposition des données sur les employés d’une entreprise.</p>"
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ api/controller/user.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, request
+from api.service.user import UserService
+
+user_blueprint = Blueprint('user', __name__)
+
+service = UserService
+
+@user_blueprint.route('/', methods=['POST'])
+def create_user():
+    pass
+
+
+@user_blueprint.route('/', methods=['GET'])
+def get_users():
+    pass
+
+
+@user_blueprint.route('/<str:username>', methods=['GET'])
+def get_user(username: str):
+    pass
+
+
+
+@user_blueprint.route('/<str:username>', methods=['PATCH'])
+def update_user(username: str):
+    pass
+
+
+@user_blueprint.route('/<str:username>', methods=['DELETE'])
+def delete_user(username: str):
+    pass
+
+
+@user_blueprint.route('/', methods=['DELETE'])
+def delete_users():
+    pass
