38161efb99eda9570c59ad1d769269d2193d9dfa
arkayzdev <franck.zhuang@hotmail.com>
2023-12-16 23:59:03 +0100

    Add AuthentificationService

---
+++ api/service/authentification.py
@@ -0,0 +1,11 @@
+from api.repository.user import UserRepo
+
+class AuthentificationService:
+    def check_user(username: str):
+        pass
+
+    def check_password(username: str):
+        pass
+        
+    def check_role(username: str):
+        pass
