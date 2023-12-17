202a0eb2b202eb6f4eba859635ac045008688a36
arkayzdev <franck.zhuang@hotmail.com>
2023-12-17 19:16:19 +0100

    Add serialize and json_fmt functions in models

--- api/model/apartment.py
+++ api/model/apartment.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Apartment:
     def __init__(self, id_apartment: int, area: int, max_people: int, address: str, availability: bool, username: str) -> None:
         self.id_apartment = id_apartment
@@ -7,5 +12,19 @@ class Apartment:
         self.availability = availability
         self.username = username
 
-
+    def serialize(self):
+        return {
+            'id_apartment': self.id_apartment,
+            'area': self.area,
+            'max_people': self.max_people,
+            'address': self.address,
+            'availability': self.availability
+        }
     
+    def json_fmt(self):
+        return {
+        'apartment': {
+            'id_apartment' : self.id_apartment,
+            'url': f'{os.getenv("hostname")}{self.id_apartment}'
+        }
+    }
--- api/model/reservation.py
+++ api/model/reservation.py
@@ -1,10 +1,29 @@
 import datetime
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 class Reservation:
     def __init__(self, id_reservation: int, start_date: datetime, end_date: datetime, price: int, username: str) -> None:
-        self.id = id_reservation
+        self.id_reservation = id_reservation
         self.start_date = start_date
         self.end_date = end_date
         self.price = price
         self.username = username
     
+    def serialize(self):
+        return {
+            'id_reservation': self.id_reservation,
+            'start_date': self.start_date,
+            'end_date': self.end_date,
+            'price': self.price,
+            'username': self.username
+        }
+    
+    def json_fmt(self):
+        return {
+            'reservation': {
+                'id_reservation': self.id_reservation,
+                'url': f'{os.getenv("hostname")}{self.id_reservation}'
+            }
+        }
